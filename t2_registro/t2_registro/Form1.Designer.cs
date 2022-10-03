9330f766009122acaa1ec34aa0107ea96cd65da4
Williams392 <williamsvalle392@gmail.com>
2022-10-03 02:56:57 -0500

    goo_1.3

---
+++ t2_registro/t2_registro/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace t2_registro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtTotatlPagar = new System.Windows.Forms.Label();
+            this.IBINombre = new System.Windows.Forms.Label();
+            this.lblt = new System.Windows.Forms.Label();
+            this.IbIPrecio = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.IBIcodigo = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.cmbProducto = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columna2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TOTAL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.btnSalir);
+            this.panel1.Controls.Add(this.txtTotatlPagar);
+            this.panel1.Controls.Add(this.IBINombre);
+            this.panel1.Controls.Add(this.lblt);
+            this.panel1.Controls.Add(this.IbIPrecio);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.btnEliminar);
+            this.panel1.Controls.Add(this.IBIcodigo);
+            this.panel1.Controls.Add(this.btnAgregar);
+            this.panel1.Controls.Add(this.txtCantidad);
+            this.panel1.Controls.Add(this.cmbProducto);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.dgvLista);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(12, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(433, 423);
+            this.panel1.TabIndex = 9;
+            // 
+            // txtTotatlPagar
+            // 
+            this.txtTotatlPagar.AutoSize = true;
+            this.txtTotatlPagar.Location = new System.Drawing.Point(108, 395);
+            this.txtTotatlPagar.Name = "txtTotatlPagar";
+            this.txtTotatlPagar.Size = new System.Drawing.Size(11, 13);
+            this.txtTotatlPagar.TabIndex = 24;
+            this.txtTotatlPagar.Text = "-";
+            // 
+            // IBINombre
+            // 
+            this.IBINombre.AutoSize = true;
+            this.IBINombre.Location = new System.Drawing.Point(108, 95);
+            this.IBINombre.Name = "IBINombre";
+            this.IBINombre.Size = new System.Drawing.Size(11, 13);
+            this.IBINombre.TabIndex = 20;
+            this.IBINombre.Text = "-";
+            // 
+            // lblt
+            // 
+            this.lblt.AutoSize = true;
+            this.lblt.Location = new System.Drawing.Point(77, 371);
+            this.lblt.Name = "lblt";
+            this.lblt.Size = new System.Drawing.Size(101, 13);
+            this.lblt.TabIndex = 23;
+            this.lblt.Text = "PRECIO TOTAL:";
+            // 
+            // IbIPrecio
+            // 
+            this.IbIPrecio.AutoSize = true;
+            this.IbIPrecio.Location = new System.Drawing.Point(108, 128);
+            this.IbIPrecio.Name = "IbIPrecio";
+            this.IbIPrecio.Size = new System.Drawing.Size(11, 13);
+            this.IbIPrecio.TabIndex = 19;
+            this.IbIPrecio.Text = "-";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "PRODUCTO:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 158);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "CANTIDAD:";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(293, 134);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(108, 37);
+            this.btnEliminar.TabIndex = 14;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // IBIcodigo
+            // 
+            this.IBIcodigo.AutoSize = true;
+            this.IBIcodigo.Location = new System.Drawing.Point(108, 62);
+            this.IBIcodigo.Name = "IBIcodigo";
+            this.IBIcodigo.Size = new System.Drawing.Size(11, 13);
+            this.IBIcodigo.TabIndex = 21;
+            this.IBIcodigo.Text = "-";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(293, 50);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(108, 37);
+            this.btnAgregar.TabIndex = 13;
+            this.btnAgregar.Text = "REGISTRAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(111, 151);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(97, 20);
+            this.txtCantidad.TabIndex = 12;
+            // 
+            // cmbProducto
+            // 
+            this.cmbProducto.FormattingEnabled = true;
+            this.cmbProducto.Items.AddRange(new object[] {
+            "Alcohol Medicinal 70% - Frasco 120 ML",
+            "Panadol Antigripal NF Tableta",
+            "Panadol Forte Tableta",
+            "Multiflora Plus Cápsula",
+            "Vick Vaporub Ungüento tópico"});
+            this.cmbProducto.Location = new System.Drawing.Point(111, 20);
+            this.cmbProducto.Name = "cmbProducto";
+            this.cmbProducto.Size = new System.Drawing.Size(176, 21);
+            this.cmbProducto.TabIndex = 11;
+            this.cmbProducto.SelectedIndexChanged += new System.EventHandler(this.cmbProducto_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "PRECIO:";
+            // 
+            // dgvLista
+            // 
+            this.dgvLista.AllowUserToAddRows = false;
+            this.dgvLista.AllowUserToDeleteRows = false;
+            this.dgvLista.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvLista.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.columna2,
+            this.Column3,
+            this.Column4,
+            this.TOTAL});
+            this.dgvLista.Location = new System.Drawing.Point(14, 193);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.ReadOnly = true;
+            this.dgvLista.RowHeadersVisible = false;
+            this.dgvLista.Size = new System.Drawing.Size(403, 163);
+            this.dgvLista.TabIndex = 10;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "CODIGO";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // columna2
+            // 
+            this.columna2.HeaderText = "PRODUCTO";
+            this.columna2.Name = "columna2";
+            this.columna2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "PRECIO";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "CANTIDAD";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // TOTAL
+            // 
+            this.TOTAL.HeaderText = "TOTAL";
+            this.TOTAL.Name = "TOTAL";
+            this.TOTAL.ReadOnly = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "NOMBRE:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "CODIGO:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(103, 18);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(261, 18);
+            this.label14.TabIndex = 28;
+            this.label14.Text = "REGISTRO DE MEDICAMENTOS";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(73, 33);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 29;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(258, 362);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(108, 46);
+            this.btnSalir.TabIndex = 25;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(463, 484);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FARMACIA";
+            this.TransparencyKey = System.Drawing.Color.Gray;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label txtTotatlPagar;
+        private System.Windows.Forms.Label IBINombre;
+        private System.Windows.Forms.Label lblt;
+        private System.Windows.Forms.Label IbIPrecio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label IBIcodigo;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.ComboBox cmbProducto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvLista;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columna2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TOTAL;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ t2_registro/t2_registro/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace t2_registro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbProducto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int p_cod;
+            string p_nombre;
+            float p_precio;
+
+            p_cod = cmbProducto.SelectedIndex;
+            p_nombre = cmbProducto.SelectedItem.ToString();
+            p_precio = cmbProducto.SelectedIndex;
+
+            switch (p_cod)
+            {
+                case 0: IBIcodigo.Text = "0011";break;
+                case 1: IBIcodigo.Text = "0022";break;
+                case 2: IBIcodigo.Text = "0033";break;
+                case 3: IBIcodigo.Text = "oo44";break;
+                default: IBIcodigo.Text = "0055";break;
+            }
+            switch (p_nombre)
+            {
+                case "Alcohol Medicinal 70% - Frasco 120 ML": IBINombre.Text = "Alcohol Medicinal 70% - Frasco 120 ML";break;
+                case "Panadol Antigripal NF Tableta": IBINombre.Text = "Panadol Antigripal NF Tableta";break;
+                case "Panadol Forte Tableta": IBINombre.Text = "Panadol Forte Tableta";break;
+                case "Multiflora Plus Cápsula": IBINombre.Text = "Multiflora Plus Cápsula"; break;
+                default: IBINombre.Text = "Vick Vaporub Ungüento tópico";break;
+            }
+            switch (p_precio)
+            {
+                case 0: IbIPrecio.Text = "1.80"; break;
+                case 1: IbIPrecio.Text = "2.20"; break;
+                case 2: IbIPrecio.Text = "1.78"; break;
+                case 3: IbIPrecio.Text = "133.80"; break;
+                default: IbIPrecio.Text = "9.00"; break;
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow file = new DataGridViewRow();
+            file.CreateCells(dgvLista);
+
+            file.Cells[0].Value = IBIcodigo.Text;
+            file.Cells[1].Value = IBINombre.Text;
+            file.Cells[2].Value = IbIPrecio.Text;
+            file.Cells[3].Value = txtCantidad.Text;
+            file.Cells[4].Value = (float.Parse(IbIPrecio.Text) * float.Parse(txtCantidad.Text)).ToString();
+
+            dgvLista.Rows.Add(file);
+
+            DialogResult r = MessageBox.Show("Si se encontró?",
+                                              "Medicamento",
+                                              MessageBoxButtons.YesNo,
+                                              MessageBoxIcon.Exclamation);
+            
+
+            IBIcodigo.Text = IBINombre.Text = txtCantidad.Text = "";
+
+
+            obtenerTotal();
+
+            
+        }
+
+        public void obtenerTotal()
+        {
+            float costot = 0;
+            int contador = 0;
+
+            contador = dgvLista.RowCount;
+
+            for (int i = 0; i < contador; i++) // Bucle
+            {
+                costot += float.Parse(dgvLista.Rows[i].Cells[4].Value.ToString());
+            }
+
+            txtTotatlPagar.Text = costot.ToString();
+        }
+
+
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult rppta = MessageBox.Show("¿Desea eliminar producto?",
+                    "Eliminacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (rppta == DialogResult.Yes)
+                {
+                    dgvLista.Rows.Remove(dgvLista.CurrentRow);
+                }
+            }
+            catch { }
+            obtenerTotal();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Esta seguro de Salir?", 
+                                              "Registro",
+                                              MessageBoxButtons.YesNo,
+                                              MessageBoxIcon.Exclamation);
+            if (r == DialogResult.Yes) this.Close();
+        }
+    }
+}
